02fd7b8e9f633c6f0ab1369731ae6ed2e8a9c4d3
dianka297 <dianamakharadze96@gmail.com>
2025-01-13 13:48:28 +0400

    add task done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { lazy, Suspense } from 'react';
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() =>
-  import('./pages/MovieDetailsPage/MovieDetailsPage')
+ import('./pages/MovieDetailsPage/MovieDetailsPage')
 );
 const NotFoundPage = lazy(() =>
   import('./pages/NotFoundPage/NotFoundPage')
