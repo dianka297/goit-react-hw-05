a785cc36456b2302cd5926895b8c0cba930bd03d
dianka297 <dianamakharadze96@gmail.com>
2025-01-13 13:32:04 +0400

    add task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
-const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() =>
-  import('../../pages/MovieDetailsPage/MovieDetailsPage')
+  import('./pages/MovieDetailsPage/MovieDetailsPage')
 );
 const NotFoundPage = lazy(() =>
-  import('../../pages/NotFoundPage/NotFoundPage')
+  import('./pages/NotFoundPage/NotFoundPage')
 );
 
 const Navigation = lazy(() => import('../Navigation/Navigation'));
